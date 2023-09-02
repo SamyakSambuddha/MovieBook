a0532d67aaf95afc1fa1dbfa32b4d0c2d0f63340
Sanket Kiran Nivalkar <87200314+SamyakSambuddha@users.noreply.github.com>
2023-09-02 22:21:16 +0530

    Delete genre.routes.js

--- genre.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const genres = require("../controllers/genre.controller.js");
-    var router = require("express").Router();
-    router.get("/", genres.findAllGenres);
-    app.use('/api/genres', router);
-    };
