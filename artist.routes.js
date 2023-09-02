2cafd4408bcd6990f0ba088bb40e1a4758cefbb8
Sanket Kiran Nivalkar <87200314+SamyakSambuddha@users.noreply.github.com>
2023-09-02 22:17:46 +0530

    Delete artist.routes.js

--- artist.routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-    const artists = require("../controllers/artist.controller.js");
-    var router = require("express").Router();
-    router.get("/", artists.findAllArtists);
-    app.use('/api/artists', router);
-    };
